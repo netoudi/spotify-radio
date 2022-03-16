2346e77e2a388e2d94e5cccad084d6f049d6e32c
netoudi <netoudi@rocketmail.com>
2022-03-15 22:13:57 -0300

    feat: add handle error

--- src/routes.js
+++ src/routes.js
@@ -39,8 +39,20 @@ async function routes(request, response) {
   return response.end();
 }
 
+function handleError(error, response) {
+  if (error.message.includes('ENOENT')) {
+    logger.warn(`asset not found ${error.stack}`);
+    response.writeHead(404);
+    return response.end();
+  }
+
+  logger.error(`caught error on API ${error.stack}`);
+  response.writeHead(500);
+  return response.end();
+}
+
 export function handler(request, response) {
   return routes(request, response).catch((error) =>
-    logger.error(`Deu ruimmmm: ${error.stack}`)
+    handleError(error, response)
   );
 }
