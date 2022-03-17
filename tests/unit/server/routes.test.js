1148eff34f324d75e958a69d2f81765366031872
netoudi <netoudi@rocketmail.com>
2022-03-16 22:45:57 -0300

    chore(tests): add test todo

--- tests/unit/server/routes.test.js
+++ tests/unit/server/routes.test.js
@@ -1,7 +1,26 @@
-import { describe, expect, test } from '@jest/globals';
+import { beforeEach, describe, test } from '@jest/globals';
+import config from '../../../src/config.js';
 
-describe('#Routes', () => {
-  test('My test', () => {
-    expect(true).toBeTruthy();
+describe('#Routes - test site for API response', () => {
+  beforeEach(() => {
+    jest.restoreAllMocks();
+    jest.clearAllMocks();
+  });
+
+  test.todo('GET / - should redirect to home page');
+  test.todo(
+    `GET /home - should response with ${config.pages.homeHTML} file stream`
+  );
+  test.todo(
+    `GET /controller - should resposne with ${config.pages.controllerHTML} file stream`
+  );
+  test.todo(`GET /file.ext - should response with file stream`);
+  test.todo(
+    `GET /unknown - given an inexistent route it should response with 404`
+  );
+
+  describe('exceptions', () => {
+    test.todo('given inexistent file it should respond with 404');
+    test.todo('given an error it should respond with 500');
   });
 });
