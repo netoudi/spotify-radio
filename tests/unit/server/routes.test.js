d90323ca9be160603e19ae343ccbf96d9e58a104
netoudi <netoudi@rocketmail.com>
2022-03-17 00:25:00 -0300

    chore(tests): implements tests to routes.js

--- tests/unit/server/routes.test.js
+++ tests/unit/server/routes.test.js
@@ -1,5 +1,8 @@
-import { beforeEach, describe, test } from '@jest/globals';
+import { beforeEach, describe, jest, test } from '@jest/globals';
 import config from '../../../src/config.js';
+import { Controller } from '../../../src/controller.js';
+import { handler } from '../../../src/routes.js';
+import TestUtil from '../../_util/testUtil.js';
 
 describe('#Routes - test site for API response', () => {
   beforeEach(() => {
@@ -7,20 +10,144 @@ describe('#Routes - test site for API response', () => {
     jest.clearAllMocks();
   });
 
-  test.todo('GET / - should redirect to home page');
-  test.todo(
-    `GET /home - should response with ${config.pages.homeHTML} file stream`
-  );
-  test.todo(
-    `GET /controller - should resposne with ${config.pages.controllerHTML} file stream`
-  );
-  test.todo(`GET /file.ext - should response with file stream`);
-  test.todo(
-    `GET /unknown - given an inexistent route it should response with 404`
-  );
+  test('GET / - should redirect to home page', async () => {
+    const params = TestUtil.defaultHandleParams();
+    params.request.method = 'GET';
+    params.request.url = '/';
+
+    await handler(...params.values());
+
+    expect(params.response.writeHead).toBeCalledWith(302, {
+      Location: config.location.home,
+    });
+    expect(params.response.end).toBeCalled();
+  });
+
+  test(`GET /home - should response with ${config.pages.homeHTML} file stream`, async () => {
+    const params = TestUtil.defaultHandleParams();
+    params.request.method = 'GET';
+    params.request.url = '/home';
+    const mockFileStream = TestUtil.generateReadableStream(['data']);
+
+    jest
+      .spyOn(Controller.prototype, Controller.prototype.getFileStream.name)
+      .mockResolvedValue({ stream: mockFileStream });
+
+    jest.spyOn(mockFileStream, 'pipe').mockReturnValue();
+
+    await handler(...params.values());
+
+    expect(Controller.prototype.getFileStream).toBeCalledWith(
+      config.pages.homeHTML
+    );
+    expect(mockFileStream.pipe).toHaveBeenCalledWith(params.response);
+  });
+
+  test(`GET /controller - should resposne with ${config.pages.controllerHTML} file stream`, async () => {
+    const params = TestUtil.defaultHandleParams();
+    params.request.method = 'GET';
+    params.request.url = '/controller';
+    const mockFileStream = TestUtil.generateReadableStream(['data']);
+
+    jest
+      .spyOn(Controller.prototype, Controller.prototype.getFileStream.name)
+      .mockResolvedValue({ stream: mockFileStream });
+
+    jest.spyOn(mockFileStream, 'pipe').mockReturnValue();
+
+    await handler(...params.values());
+
+    expect(Controller.prototype.getFileStream).toBeCalledWith(
+      config.pages.controllerHTML
+    );
+    expect(mockFileStream.pipe).toHaveBeenCalledWith(params.response);
+  });
+
+  test(`GET /index.html - should response with file stream`, async () => {
+    const params = TestUtil.defaultHandleParams();
+    const filename = 'index.html';
+    params.request.method = 'GET';
+    params.request.url = filename;
+    const expectedType = '.html';
+    const mockFileStream = TestUtil.generateReadableStream(['data']);
+
+    jest
+      .spyOn(Controller.prototype, Controller.prototype.getFileStream.name)
+      .mockResolvedValue({ stream: mockFileStream, type: expectedType });
+
+    jest.spyOn(mockFileStream, 'pipe').mockReturnValue();
+
+    await handler(...params.values());
+
+    expect(Controller.prototype.getFileStream).toBeCalledWith(filename);
+    expect(mockFileStream.pipe).toHaveBeenCalledWith(params.response);
+    expect(params.response.writeHead).toHaveBeenCalledWith(200, {
+      'Content-Type': config.constants.CONTENT_TYPE[expectedType],
+    });
+  });
+
+  test(`GET /file.ext - should response with file stream`, async () => {
+    const params = TestUtil.defaultHandleParams();
+    const filename = 'file.ext';
+    params.request.method = 'GET';
+    params.request.url = filename;
+    const mockFileStream = TestUtil.generateReadableStream(['data']);
+
+    jest
+      .spyOn(Controller.prototype, Controller.prototype.getFileStream.name)
+      .mockResolvedValue({ stream: mockFileStream });
+
+    jest.spyOn(mockFileStream, 'pipe').mockReturnValue();
+
+    await handler(...params.values());
+
+    expect(Controller.prototype.getFileStream).toBeCalledWith(filename);
+    expect(mockFileStream.pipe).toHaveBeenCalledWith(params.response);
+    expect(params.response.writeHead).not.toHaveBeenCalled();
+  });
+
+  test(`POST /unknown - given an inexistent route it should response with 404`, async () => {
+    const params = TestUtil.defaultHandleParams();
+    params.request.method = 'POST';
+    params.request.url = '/unknown';
+
+    await handler(...params.values());
+
+    expect(params.response.writeHead).toHaveBeenCalledWith(404);
+    expect(params.response.end).toHaveBeenCalled();
+  });
 
   describe('exceptions', () => {
-    test.todo('given inexistent file it should respond with 404');
-    test.todo('given an error it should respond with 500');
+    test('given inexistent file it should respond with 404', async () => {
+      const params = TestUtil.defaultHandleParams();
+      params.request.method = 'GET';
+      params.request.url = '/index.png';
+
+      jest
+        .spyOn(Controller.prototype, Controller.prototype.getFileStream.name)
+        .mockRejectedValue(
+          new Error('Error: ENOENT: no such file or directory....')
+        );
+
+      await handler(...params.values());
+
+      expect(params.response.writeHead).toHaveBeenCalledWith(404);
+      expect(params.response.end).toHaveBeenCalled();
+    });
+
+    test('given an error it should respond with 500', async () => {
+      const params = TestUtil.defaultHandleParams();
+      params.request.method = 'GET';
+      params.request.url = '/index.png';
+
+      jest
+        .spyOn(Controller.prototype, Controller.prototype.getFileStream.name)
+        .mockRejectedValue(new Error('Error...'));
+
+      await handler(...params.values());
+
+      expect(params.response.writeHead).toHaveBeenCalledWith(500);
+      expect(params.response.end).toHaveBeenCalled();
+    });
   });
 });
